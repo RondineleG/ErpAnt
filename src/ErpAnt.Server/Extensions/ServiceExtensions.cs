6b88d93d92245a8a5b7a5bac91494f9b607bc97d
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-11-26 11:18:03 -0300

    add cors policy

--- src/ErpAnt.Server/Extensions/ServiceExtensions.cs
+++ src/ErpAnt.Server/Extensions/ServiceExtensions.cs
@@ -2,7 +2,18 @@ namespace ErpAnt.Server.Extensions
 {
     public static class ServiceExtensions
     {
+         public static void ConfigureCors(this IserviceCollection services)
+         {
 
+             services.AddCors(options =>
+             {
+                 options.AddPolicy("CorsPolicy",
+                 builder => builder.AllowAnyOrigin()
+                 .AllowMethod()
+                 .AllowHeader());
+             });
+
+         }
             
     }
 }
