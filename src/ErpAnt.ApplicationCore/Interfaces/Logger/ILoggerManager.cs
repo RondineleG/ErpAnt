172f39cd6130cb33dc932989fb7b0ae102893b5a
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-11-26 12:11:30 -0300

    Renamed

--- src/ErpAnt.ApplicationCore/Interfaces/Logger/ILoggerManager.cs
+++ src/ErpAnt.ApplicationCore/Interfaces/Logger/ILoggerManager.cs
@@ -1,6 +1,6 @@
 namespace ErpAnt.ApplicationCore.Interfaces.Logger
 {
-    public interface ILoggerManger
+    public interface ILoggerManager
     {
          void LogInformation(string message);
          void LogWarning(string message);
