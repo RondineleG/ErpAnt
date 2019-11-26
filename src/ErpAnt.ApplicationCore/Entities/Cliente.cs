464a70e0efc6b631967dddc89b0b9d9144fffbbf
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-11-26 09:29:18 -0300

    Criando classes base da AplicationCore

---
+++ src/ErpAnt.ApplicationCore/Entities/Cliente.cs
@@ -0,0 +1,19 @@
+namespace ErpAnt.ApplicationCore.Entities 
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            
+        }
+        public int ClienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Telefone { get; set; }         
+
+        public string Email { get; set; }
+
+         
+    }
+}
