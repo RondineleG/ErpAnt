0b2a1a770fe3c44689da74f0bbaed1172a8490ff
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-11-26 11:26:02 -0300

    Using extensions methods in startup

--- src/ErpAnt.Server/Startup.cs
+++ src/ErpAnt.Server/Startup.cs
@@ -1,3 +1,4 @@
+using ErpAnt.Server.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,8 @@ namespace ErpAnt.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
             services.AddControllers();
         }
 
