9d65cec7e348e8652ba7caca0d34c26fe02b8bfa
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-11-28 11:06:42 -0300

    Creating method for controller

--- src/ErpAnt.Server/Controllers/CustomerController.cs
+++ src/ErpAnt.Server/Controllers/CustomerController.cs
@@ -1,10 +1,18 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ErpAnt.Server.Controllers
 {
     public class CustomerController : ControllerBase
 
-    {
+    {      
+       // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            
+            return new string[] { "value1", "value2" };
+        }
         
     }
 }
