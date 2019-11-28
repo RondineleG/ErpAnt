e9e71efa514876dbb556fc5d2436cb072a36bdff
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-11-28 11:20:19 -0300

    Referencia to Interface

--- src/ErpAnt.InfraStructure/Repositories/Repository.cs
+++ src/ErpAnt.InfraStructure/Repositories/Repository.cs
@@ -1,9 +1,35 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
 using ErpAnt.ApplicationCore.Interfaces.Repositories;
 
 namespace ErpAnt.InfraStructure.Repositories
 {
-    public class Repository : IRepository
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
     {
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
