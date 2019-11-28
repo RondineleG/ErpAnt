b9aa1ceaaa70566fe4e774832511740cfcc5d117
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-11-28 10:46:14 -0300

    Add  Data Annotations to Customer

--- src/ErpAnt.ApplicationCore/Models/Customer.cs
+++ src/ErpAnt.ApplicationCore/Models/Customer.cs
@@ -1,22 +1,32 @@
+using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace ErpAnt.ApplicationCore.Models
 {
     public class Customer : ModelBase
     {
         public Customer()
         {
-            
+            CreatedDate = DateTime.Now;
         }
+
+        [Required(ErrorMessage = "The Name is required")]
+        [StringLength(60, ErrorMessage = "Name can't be longer than 60 characters")]
         public string Name { get; set; }
 
         public string Phone { get; set; }         
 
+
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(100, ErrorMessage = "Email can't be longer than 50 characters")]
         public string Email { get; set; }
 
+        public DateTime CreatedDate { get; set; }
+
         /* EF Relations */
-        public IEnumerable<Order> Orders { get; set; }
+        public ICollection<Order> Orders { get; set; }
 
          
     }
 }
+  
