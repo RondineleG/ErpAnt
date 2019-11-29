b96c99e33d2d4b4b3afc19a6089d89b0907de5d2
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-11-29 09:16:57 -0300

    Create class Category

---
+++ src/ErpAnt.ApplicationCore/Models/Category.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ErpAnt.ApplicationCore.Models
+{
+    [Table ("Category")]
+    public class Category : ModelBase
+    {
+
+    }
+}
